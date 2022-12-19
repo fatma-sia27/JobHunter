d4b2317ef97fb43111b86a3ef14a20be526e7c96
aasia fatma <fatma.sia2020@gmail.com>
2022-12-19 09:50:03 +0530

    Updates

---
+++ precache-manifest.887988c07bfd0963ba0af3f0a9ffca5a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "773200aaa4d791b7a42e",
+    "url": "/jobHunter/static/css/main.7cfdbafb.chunk.css"
+  },
+  {
+    "revision": "773200aaa4d791b7a42e",
+    "url": "/jobHunter/static/js/main.5025c220.chunk.js"
+  },
+  {
+    "revision": "62d5234422549055f8f3",
+    "url": "/jobHunter/static/js/runtime~main.38ae4dbf.js"
+  },
+  {
+    "revision": "85b7afef6a1eb791ac8d",
+    "url": "/jobHunter/static/js/2.944aa68d.chunk.js"
+  },
+  {
+    "revision": "d14e4b2bc92b6ae455e9",
+    "url": "/jobHunter/static/js/3.4ea34601.chunk.js"
+  },
+  {
+    "revision": "b34edbefc21f2ce671baca9084593ad0",
+    "url": "/jobHunter/static/media/bgImg.b34edbef.jpg"
+  },
+  {
+    "revision": "b013bd2f318af2815c74072242c5befb",
+    "url": "/jobHunter/static/media/applyMain.b013bd2f.png"
+  },
+  {
+    "revision": "df3432b488f4fa735aa6016d25e82946",
+    "url": "/jobHunter/static/media/joinMain.df3432b4.png"
+  },
+  {
+    "revision": "2738af19fecc1e159164e34f85662b42",
+    "url": "/jobHunter/static/media/intro_pic1.2738af19.png"
+  },
+  {
+    "revision": "14e69bdd0346b5da183d38948b2ed5c0",
+    "url": "/jobHunter/static/media/main_2ndImage.14e69bdd.png"
+  },
+  {
+    "revision": "3645a8b52c64c733a8c8fe97ec84000d",
+    "url": "/jobHunter/static/media/About_img.3645a8b5.png"
+  },
+  {
+    "revision": "e4d3b895098718209107357f1def91ff",
+    "url": "/jobHunter/static/media/aasPic.e4d3b895.png"
+  },
+  {
+    "revision": "de7f873ac32767a604ff0189509ca439",
+    "url": "/jobHunter/static/media/tannuPic.de7f873a.png"
+  },
+  {
+    "revision": "28c4c00e0001def77daeb1c65bb71053",
+    "url": "/jobHunter/static/media/indus_login.28c4c00e.png"
+  },
+  {
+    "revision": "b5b651ab0ba7432d58e70a57cc4ac431",
+    "url": "/jobHunter/static/media/stud_login.b5b651ab.png"
+  },
+  {
+    "revision": "d242747193351d9ac0c03cf276291c62",
+    "url": "/jobHunter/static/media/contactUsBg.d2427471.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/jobHunter/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "72e486aeaf292aa41b0e73f743cfe9e6",
+    "url": "/jobHunter/static/media/recruiter_img.72e486ae.png"
+  },
+  {
+    "revision": "7c0552d5b5ec95029b0b8d4a8b85a544",
+    "url": "/jobHunter/index.html"
+  }
+];
